9eb3c74553b729c5fcc5b21804ee95b00abb502c
sujan123345 <sujansharma7705@gmail.com>
2026-03-03 12:16:22 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,6 +1,6 @@
 public class code {
     public static void main(String[] args) {
-        String str = null;
+        String str = Sujan;
         
        
         System.out.println(str.length());
