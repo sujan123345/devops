9a388dd2506078498948af6046015306e7c87184
sujan123345 <sujansharma7705@gmail.com>
2026-03-09 10:20:12 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,6 +1,6 @@
 public class code {
     public static void main(String[] args) {
-        String str = "Sujan2";
+        String str = "Sujan3";
         System.out.println(str.length());
     }
 }
